bf2847f90ac37e506b5ca1bcc84a1467fcc77519
Nail Gafarov <nil20122013@gmail.com>
2022-11-03 18:55:04 +0500

    Completed basic dropdown functionality

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -1,97 +1,116 @@
+// TODO: Replace an item searcing in array with searching in trie
 import React from "react";
 
+/**
+ * Throws an error if {@link Props.defaultValue} is not contained in {@link Props.children}.
+ */
 export default class Dropdown extends React.PureComponent<Props, State> {
+
+	private static readonly STYLE: React.CSSProperties = {
+		position: "relative"
+	};
+
+	private static readonly LIST_STYLE: React.CSSProperties = {
+		position: "absolute",
+		left: "0",
+		top: "100%",
+		width: "100%",
+		listStyle: "none"
+	};
 
 	private get className(): string {
 		const className = [
-			"dropdown"
+			"dropdown", this.state.state
 		];
 		if (this.props.className)
 			className.push(this.props.className);
 		return className.join(" ");
 	}
 
-	private get listStyle(): React.CSSProperties {
-		return {
-			display: this.state.listVisible ? "block" : "none",
-			position: "absolute",
-			left: "0",
-			top: "100%",
-			width: "100%"
-		};
-	}
-
 	public constructor(props: Props) {
 		super(props);
+		if (props.defaultValue && !props.children.includes(props.defaultValue))
+			throw new Error(`The value "${props.defaultValue}" of props.defaultValue does not match any of the entries in props.children array`);
 		this.state = {
-			listVisible: false,
-			value: this.props.defaultValue ?? "",
-			pointerInside: true
+			value: props.defaultValue ?? "",
+			items: props.children,
+			state: "collapsed",
+			pointerInside: false
 		};
-		this.onPointerEnter = this.onPointerEnter.bind(this);
-		this.onPointerLeave = this.onPointerLeave.bind(this);
-		this.onFocus = this.onFocus.bind(this);
-		this.onBlur = this.onBlur.bind(this);
-		this.onInput = this.onInput.bind(this);
-		this.onItemClick = this.onItemClick.bind(this);
 	}
 
 	public render(): React.ReactNode {
 		return (
-			<div className={this.className} onPointerEnter={this.onPointerEnter} onPointerLeave={this.onPointerLeave}>
-				<input className="dropdown-input" type="text" placeholder={this.props.placeholder} value={this.state.value} onFocus={this.onFocus} onBlur={this.onBlur} onInput={this.onInput} />
-				<div className="dropdown-list" style={this.listStyle}>
-					{this.props.children.map(item => (
-						<div key={item} className="dropdown-item" onClick={this.onItemClick} data-value={item} dangerouslySetInnerHTML={{__html: "" /* TODO */}} />
-					))}
-				</div>
+			<div className={this.className} style={Dropdown.STYLE} onPointerEnter={this.onPointerEnter} onPointerLeave={this.onPointerLeave}>
+				<input type="text" placeholder={this.props.placeholder} value={this.state.value} onFocus={this.onFocus} onBlur={this.onBlur} onInput={this.onInput} />
+				{this.state.state === "expanded" && (
+					<ul style={Dropdown.LIST_STYLE}>
+						{this.state.items.map(item => (
+							<li key={item} onClick={this.onItemClick} data-value={item} dangerouslySetInnerHTML={{__html: Dropdown.highlight(this.state.value, item)}} />
+						))}
+					</ul>
+				)}
 			</div>
 		);
 	}
 
-	private onPointerEnter(e: React.SyntheticEvent<HTMLDivElement, PointerEvent>): void {
+	private onPointerEnter = (): void => {
 		this.setState({
 			pointerInside: true
 		});
 	}
 
-	private onPointerLeave(e: React.SyntheticEvent<HTMLDivElement, PointerEvent>): void {
+	private onPointerLeave = (): void => {
 		this.setState({
 			pointerInside: false
 		});
 	}
 
-	private onFocus(e: React.SyntheticEvent<HTMLInputElement, FocusEvent>): void {
+	private onFocus = (): void => {
 		this.setState({
-			listVisible: true
+			state: "expanded"
 		});
 	}
 
-	// TODO
-	private onBlur(e: React.SyntheticEvent<HTMLInputElement, FocusEvent>): void {
+	private onBlur = (): void => {
 		if (!this.state.pointerInside)
 			this.setState({
-				listVisible: false
+				state: "collapsed"
 			});
 	}
 
-	// TODO
-	private onInput(e: React.SyntheticEvent<HTMLInputElement, InputEvent>): void {
+	private onInput = (e: React.SyntheticEvent<HTMLInputElement, InputEvent>): void => {
 		const target = e.target as HTMLInputElement;
-		const lcValue = target.value.toLowerCase();
+		const words = Dropdown.splitToWords(target.value);
+		const items = this.props.children.filter(item => words.some(w => item.toLowerCase().includes(w)));
 		this.setState({
-			value: target.value
+			value: target.value,
+			items
 		});
+		this.props.onChange?.(target.value, !!items.length);
 	}
 
-	// TODO
-	private onItemClick(e: React.SyntheticEvent<HTMLDivElement, MouseEvent>): void {
+	private onItemClick = (e: React.SyntheticEvent<HTMLLIElement, MouseEvent>): void => {
 		const target = e.target as HTMLDivElement;
 		const value = target.getAttribute("data-value")!;
 		this.setState({
-			listVisible: false,
-			value
+			state: "collapsed",
+			value,
+			items: [
+				value
+			]
 		});
+		this.props.onChange?.(value, true);
+	}
+
+	private static highlight(input: string, string: string): string {
+		const words = Dropdown.splitToWords(input);
+		const regex = new RegExp(words.join("|"), "gui");
+		return string.replace(regex, "<span class=\"hl\">$&</span>");
+	}
+
+	private static splitToWords(input: string): string[] {
+		return input.toLowerCase().split(/\s+/g);
 	}
 }
 
@@ -108,22 +127,26 @@ type Props = {
 	placeholder?: string;
 
 	/**
-	 * Default value. Should be one of the values included in {@link Props.data}.
+	 * Default value. Should be one of the values included in {@link Props.children}.
 	 */
 	defaultValue?: string;
 
 	/**
-	 * CSS-class.
+	 * CSS class.
 	 */
 	className?: string;
 
-	// TODO: onChange?(value: string): void;
+	/**
+	 * Fires each time input changes it's value.
+	 * @param value Current input's value.
+	 * @param valid `true` if the value matches at least one item in the {@link Props.children}.
+	 */
+	onChange?(value: string, valid: boolean): void;
 }
 
 type State = {
 	value: string;
-	listVisible: boolean;
+	items: string[];
+	state: "expanded" | "collapsed";
 	pointerInside: boolean;
 }
-
-// TODO: Replace an item searcing in array with searching in trie
