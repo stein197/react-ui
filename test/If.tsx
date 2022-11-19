d7628d5bdfcdada5834ddcba1268361f9ff150c9
Nail Gafarov <nil20122013@gmail.com>
2022-11-19 17:12:13 +0500

    Moved the boilerplate out

--- test/If.tsx
+++ test/If.tsx
@@ -5,118 +5,88 @@ import {act} from "react-dom/test-utils";
 import * as jsdom from "jsdom";
 import * as mocha from "mocha";
 import {If, Then, Else} from "../src/If";
+import setup from "./setup";
 
-// TODO: Move the boilerplate setup outside
 mocha.describe("<If />", () => {
-	let container: HTMLDivElement
-	let root: Root;
-	const oldWindow = global.window;
-	const oldDocument = global.document;
-	mocha.before(() => {
-		global.IS_REACT_ACT_ENVIRONMENT = true
-		const dom = new jsdom.JSDOM("", {
-			url: "http://localhost"
-		});
-		// @ts-ignore
-		global.window = dom.window;
-		global.document = dom.window.document;
-		container = document.createElement("div");
-		document.body.appendChild(container);
-	});
-	mocha.after(() => {
-		document.body.removeChild(container);
-		global.window = oldWindow;
-		global.document = oldDocument;
-		delete global.IS_REACT_ACT_ENVIRONMENT;
-	});
-	mocha.beforeEach(() => {
-		act(() => {
-			root = createRoot(container);
-		});
-	});
-	mocha.afterEach(() => {
-		act(() => {
-			root.unmount();
-		});
-	});
+	const sandbox = setup();
 	mocha.it("Should return children when value is true and there are no <Then /> or <Else /> clauses", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={true}>yes</If>
 			);
 		});
-		assert.equal(container.textContent, "yes");
+		assert.equal(sandbox.container.textContent, "yes");
 	});
 	mocha.it("Should not return children when value is false and there are no <Then /> or <Else /> clauses", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={false}>yes</If>
 			);
 		});
-		assert.equal(container.textContent, "");
+		assert.equal(sandbox.container.textContent, "");
 	});
 	mocha.it("Should return only <Then /> clause when value is true and there are no more children", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={true}>
 					<Then>yes</Then>
 				</If>
 			);
 		});
-		assert.equal(container.textContent, "yes");
+		assert.equal(sandbox.container.textContent, "yes");
 	});
 	mocha.it("Should return only <Then /> clause when value is true and there are other children", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={true}>
 					<Then>yes</Then>
 					<div>garbage</div>
 				</If>
 			);
 		});
-		assert.equal(container.textContent, "yes");
+		assert.equal(sandbox.container.textContent, "yes");
 	});
 	mocha.it("Should return only <Else /> clause when value is false and there are no more children", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={false}>
 					<Else>no</Else>
 				</If>
 			);
 		});
-		assert.equal(container.textContent, "no");
+		assert.equal(sandbox.container.textContent, "no");
 	});
 	mocha.it("Should return only <Else /> clause when value is false and there are other children", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={false}>
 					<Else>no</Else>
 					<div>garbage</div>
 				</If>
 			);
 		});
-		assert.equal(container.textContent, "no");
+		assert.equal(sandbox.container.textContent, "no");
 	});
 	mocha.it("Should return only <Then /> clause when value is true and there are only <Then /> and <Else /> components", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={true}>
 					<Then>yes</Then>
 					<Else>no</Else>
 				</If>
 			);
 		});
-		assert.equal(container.textContent, "yes");
+		assert.equal(sandbox.container.textContent, "yes");
 	});
 	mocha.it("Should return only <Else /> clause when value is false and there are only <Then /> and <Else /> components", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<If value={false}>
 					<Then>yes</Then>
 					<Else>no</Else>
 				</If>
 			);
 		});
-		assert.equal(container.textContent, "no");
+		assert.equal(sandbox.container.textContent, "no");
 	});
 });
--- test/Switch.tsx
+++ test/Switch.tsx
@@ -1,47 +1,15 @@
 import * as assert from "assert";
 import * as React from "react";
-import {Root, createRoot} from "react-dom/client";
 import {act} from "react-dom/test-utils";
-import * as jsdom from "jsdom";
 import * as mocha from "mocha";
 import {Switch, Case, Default} from "../src/Switch";
+import setup from "./setup";
 
-// TODO: Move the boilerplate setup outside
 mocha.describe("<Switch />", () => {
-	let container: HTMLDivElement
-	let root: Root;
-	const oldWindow = global.window;
-	const oldDocument = global.document;
-	mocha.before(() => {
-		global.IS_REACT_ACT_ENVIRONMENT = true
-		const dom = new jsdom.JSDOM("", {
-			url: "http://localhost"
-		});
-		// @ts-ignore
-		global.window = dom.window;
-		global.document = dom.window.document;
-		container = document.createElement("div");
-		document.body.appendChild(container);
-	});
-	mocha.after(() => {
-		document.body.removeChild(container);
-		global.window = oldWindow;
-		global.document = oldDocument;
-		delete global.IS_REACT_ACT_ENVIRONMENT;
-	});
-	mocha.beforeEach(() => {
-		act(() => {
-			root = createRoot(container);
-		});
-	});
-	mocha.afterEach(() => {
-		act(() => {
-			root.unmount();
-		});
-	});
+	const sandbox = setup();
 	mocha.it("Should return the matched case", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<Switch value="2">
 					<Case value="1">First</Case>
 					<Case value="2">Second</Case>
@@ -51,11 +19,11 @@ mocha.describe("<Switch />", () => {
 				</Switch>
 			);
 		});
-		assert.equal(container.textContent, "Second");
+		assert.equal(sandbox.container.textContent, "Second");
 	});
 	mocha.it("Should return the first matched case when there are more than one equal cases", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<Switch value="2">
 					<Case value="1">First</Case>
 					<Case value="2">Second</Case>
@@ -65,11 +33,11 @@ mocha.describe("<Switch />", () => {
 				</Switch>
 			);
 		});
-		assert.equal(container.textContent, "Second");
+		assert.equal(sandbox.container.textContent, "Second");
 	});
 	mocha.it("Should return nothing when there were no matches and there is no default clause", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<Switch value="0">
 					<Case value="1">First</Case>
 					<Case value="2">Second</Case>
@@ -78,11 +46,11 @@ mocha.describe("<Switch />", () => {
 				</Switch>
 			);
 		});
-		assert.equal(container.textContent, "");
+		assert.equal(sandbox.container.textContent, "");
 	});
 	mocha.it("Should return default clause when there were no matches and there is a default clause", () => {
 		act(() => {
-			root.render(
+			sandbox.root.render(
 				<Switch value="0">
 					<Case value="1">First</Case>
 					<Case value="2">Second</Case>
@@ -92,6 +60,6 @@ mocha.describe("<Switch />", () => {
 				</Switch>
 			);
 		});
-		assert.equal(container.textContent, "None");
+		assert.equal(sandbox.container.textContent, "None");
 	});
 });
