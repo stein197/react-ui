4e8d608c4fcb56385a2effb8aea2c8d2319f9037
Nail Gafarov <nil20122013@gmail.com>
2022-12-26 21:09:28 +0500

    Added function support for useAsync() hook

--- src/hook.ts
+++ src/hook.ts
@@ -10,25 +10,61 @@ import type * as type from "./type";
  * @example
  * ```tsx
  * function Component() {
- * 	const [state, value, error] = useAsync(new Promise(resolve => setTimeout(resolve, 1000)));
+ * 	const {state, value, error} = useAsync(new Promise(resolve => setTimeout(resolve, 1000)));
  * 	return (
  * 		<div>State: {state}, Value: {value}</div>
  * 	);
  * }
  * ```
  */
-export function useAsync<T, U = any>(promise: Promise<T>): type.UseAsync<T, U> {
+export function useAsync<T, U = any>(promise: Promise<T>): type.UseAsync<T, U>;
+
+/**
+ * React hook for promise.
+ * @param f Function that should return promise.
+ * @param run Should the function be called immediatly. If so, additional `run` property will be returned from the hook
+ *            that can be used to trigger promise to execute. `true` by default.
+ * @returns Promise state, promise value, promise error if any and function to execute if `{@link run} === false`.
+ * @example
+ * ```tsx
+ * function Component() {
+ * 	const {state, value, error, run} = useAsync(() => new Promise(resolve => setTimeout(resolve, 1000)), false);
+ * 	React.useEffect(() => run(), []);
+ * 	return (
+ * 		<div>State: {state}, Value: {value}</div>
+ * 	);
+ * }
+ * ```
+ */
+export function useAsync<T, U = any>(f: () => Promise<T>, run: boolean): type.UseAsync<T, U>;
+
+export function useAsync<T, U = any>(a: Promise<T> | (() => Promise<T>), run: boolean = true): type.UseAsync<T, U> {
 	const [state, setState] = React.useState<PromiseState>(PromiseState.Pending);
 	const [value, setValue] = React.useState<T>();
 	const [error, setError] = React.useState<U>();
-	React.useEffect((): void => {
-		promise.then(value => {
+	const isPromise = a instanceof Promise;
+	const runCallback = isPromise || run ? undefined : React.useCallback((): void => {
+		a().then(value => {
 			setState(PromiseState.Fulfilled);
 			setValue(value);
 		}).catch(error => {
 			setState(PromiseState.Rejected);
 			setError(error);
 		});
-	}, [promise]);
-	return {state, value, error};
+	}, [a]);
+	React.useEffect((): void => {
+		if (!isPromise || !run)
+			return;
+		a.then(value => {
+			setState(PromiseState.Fulfilled);
+			setValue(value);
+		}).catch(error => {
+			setState(PromiseState.Rejected);
+			setError(error);
+		});
+	}, [a]);
+	const result: type.UseAsync<T, U> = {state, value, error};
+	if (!isPromise && !run)
+		result.run = runCallback;
+	return result;
 }
--- src/type.ts
+++ src/type.ts
@@ -1,7 +1,8 @@
 import type PromiseState from "@stein197/util/PromiseState";
 
-export type UseAsync<T, U = any> = {
+export type UseAsync<T, U> = {
 	state: PromiseState;
 	value?: T;
 	error?: U;
+	run?(): void;
 }
