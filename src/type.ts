3bdcadd4462061796bbf06eafcf7bbce5968ff24
Nail Gafarov <nil20122013@gmail.com>
2022-12-26 21:35:08 +0500

    Added documentation to hooks return types

--- src/type.ts
+++ src/type.ts
@@ -1,13 +1,37 @@
 import type PromiseState from "@stein197/util/PromiseState";
 
 export type UseAsync<T, U> = {
+
+	/**
+	 * State of the promise.
+	 */
 	state: PromiseState;
+
+	/**
+	 * Value of the fulfilled promise.
+	 */
 	value?: T;
+
+	/**
+	 * Error of the rejected promise.
+	 */
 	error?: U;
+
+	/**
+	 * Runs promise.
+	 */
 	run?(): void;
 }
 
 export type UseToggle = {
+
+	/**
+	 * State of the toggle.
+	 */
 	state: boolean;
+
+	/**
+	 * Toggles the state.
+	 */
 	toggle(): void;
 }
